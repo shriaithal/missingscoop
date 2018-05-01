db15c6acb4e5a43ba7476f212b1b168e6f8451f4
Mojdeh <MojdehKeykhanzadeh@fico.com>
2018-04-30 22:43:54 -0700

    implemeted API test GET scenario

--- MissingScoop_APITest/src/main/java/edu/sjsu/missingscoop_APITest/TestGETSenarios.java
+++ MissingScoop_APITest/src/main/java/edu/sjsu/missingscoop_APITest/TestGETSenarios.java
@@ -1,5 +1,27 @@
 package edu.sjsu.missingscoop_APITest;
+import  edu.sjsu.missingscoop_APITest.restUtil;
+import org.testng.Assert;
 
-public class TestGETSenarios {
+import static org.junit.Assert.assertTrue;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+import javax.ws.rs.core.Response.Status;
+import org.apache.http.HttpResponse;
+import org.testng.annotations.Test;
+/**
+ * @Author MojdehKeykhanzadeh
+ */
+public class TestGETSenarios extends restUtil{
+	private String envURL = "<environment url>";
+//	private final restUtil restUtil;
+	public TestGETSenarios(){
+		
+	}
+ @Test(description= "GET /fetch/device/product?userName=?",priority=0)
+ public void getDeviceProductMap() throws Exception {
+	 HttpResponse response = doGet(envURL+"/fetch/device/product?userName=<username>", null);
+	 int statusCode =response.getStatusLine().getStatusCode();
+     Assert.assertEquals(statusCode,Status.OK.getStatusCode());
+ }
 
 }
--- MissingScoop_APITest/src/main/java/edu/sjsu/missingscoop_APITest/restUtil.java
+++ MissingScoop_APITest/src/main/java/edu/sjsu/missingscoop_APITest/restUtil.java
@@ -1,5 +1,28 @@
 package edu.sjsu.missingscoop_APITest;
 
+import java.io.IOException;
+import java.util.Collection;
+
+import org.apache.http.Header;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+
 public class restUtil {
+	
+	public HttpResponse doGet (String URI,Header header) throws Exception{
+		HttpClient httpClient = HttpClientBuilder.create().build();
+		HttpGet getRequest = new HttpGet(URI);
+		 getRequest.addHeader(header);
+		 HttpResponse response = httpClient.execute(getRequest);
+		return response;
+		
+	}
+	
+	
 
 }
